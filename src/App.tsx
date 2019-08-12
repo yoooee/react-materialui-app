730c36fa1bf5ebe05c8280d83150d0cdc2d806f2
Josef Staroba <jstaroba@centene.com>
2019-08-12 15:01:21 -0500

    Add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import CssBaseLine from '@material-ui/core/CssBaseLine';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import './App.css';
 
 const App: React.FC = (): JSX.Element => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = (): void => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
-    <CssBaseLine>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <CssBaseLine />
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </CssBaseLine>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Grid container>
+        <Grid xs={12}>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
