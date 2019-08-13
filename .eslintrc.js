d5ec78e6732964deb0b34d043e4c39f62af71201
Josef Staroba <jstaroba@centene.com>
2019-08-13 10:04:43 -0500

    Get Responsive drawer to work

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2018,
@@ -20,6 +21,13 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'no-console': ['error', { allow: ['warn'] }],
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
+      }
+    ]
   }
 };
